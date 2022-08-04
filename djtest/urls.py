86c2c549ef32ea0b452371b53b3148e3b569a616
lg550055 <lg550055@hotmail.com>
2022-08-04 12:35:40 -0500

    Adds pages gen views and templates

--- djtest/urls.py
+++ djtest/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
