8d3039bba66f14541b5347a54f60f0e7112d6f48
lg550055 <lg550055@hotmail.com>
2022-08-04 13:29:45 -0500

    Adds 3 unit tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
+# TestCase allows us to mock queries to the database
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+
+class PostTests(TestCase):
+  @classmethod
+  def setUpTestData(cls) -> None:
+    cls.post = Post.objects.create(text='This is a test!')
+
+  def test_model_content(self):
+    self.assertEqual(self.post.text, 'This is a test!')
+
+  def test_url_at_correct_location(self):
+    response = self.client.get('/posts/')
+    self.assertEqual(response.status_code, 200)
+  
+  def test_homepage(self):
+    response = self.client.get(reverse('posts'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'posts/posts.html')
+    self.assertContains(response, 'This is a test!')
+  
